ec952a250e503f2a2d615a9e1f13d28f623e7c42
голубев дима <maska.l.v@mail.ru>
2024-10-22 19:36:12 +0500

    Task1

---
+++ Tyuiu.GolubevDS.Sprint3.Task1.V25.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.GolubevDS.Sprint3.Task1.V25.Lib;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+public class DataService : ISprint3Task1V25
+{
+    public double GetMultiplySeries(int value, int startValue, int stopValue)
+    {
+        double multSeries = 1;
+        while (startValue <= stopValue)
+        {
+            multSeries = multSeries + (Math.Pow(value, startValue) - (1/4))*Math.Cos(5) ;
+        }
+        return Math.Round(multSeries, 3);
+    }
+}
---
+++ Tyuiu.GolubevDS.Sprint3.Task1.V25.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+namespace Tyuiu.GolubevDS.Sprint3.Task1.V25.Test;
+using Tyuiu.GolubevDS.Sprint3.Task1.V25.Lib;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void ValidGetMultiplySeries()
+    {
+        DataService ds = new DataService();
+
+        int value = 2;
+        int startValue = 1;
+        int stopValue = 6;
+         
+        double res = ds.GetMultiplySeries(value, startValue, stopValue);
+        double wait = 35.186;
+        Assert.AreEqual(res, wait);
+
+    }
+}
